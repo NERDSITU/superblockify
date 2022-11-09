3e68fd82531f093f40fd01242b71836dcaa941f0
Carlson Moses Büth <c_buet05@uni-muenster.de>
2022-11-09 17:43:57 +0100

    Added environment files

---
+++ environmentSetupVersionless.sh
@@ -0,0 +1,3 @@
+conda config --prepend channels conda-forge
+conda create -n OSMnxPyrLab --strict-channel-priority osmnx pyrosm jupyterlab
+conda install -c conda-forge jupyterlab_code_formatter black isort
