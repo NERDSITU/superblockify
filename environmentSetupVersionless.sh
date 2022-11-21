ce9e8d06b017c263d15c1985816a411d69582137
Carlson Moses Büth <c_buet05@uni-muenster.de>
2022-11-21 10:49:42 +0100

    Use Github action to activate versioned `environment.yaml`.

--- environmentSetupVersionless.sh
+++ environmentSetupVersionless.sh
@@ -1,3 +1,4 @@
 conda config --prepend channels conda-forge
 conda create -n OSMnxPyrLab --strict-channel-priority osmnx pyrosm jupyterlab
+conda activate OSMnxPyrLab
 conda install -c conda-forge jupyterlab_code_formatter black isort pylint pytest
