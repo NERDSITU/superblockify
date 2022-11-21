850d6c4212a44b3fd24e5720aaf7c23d9a4120e9
Carlson Moses Büth <c_buet05@uni-muenster.de>
2022-11-21 10:20:36 +0100

    Added pylint to conda `environmentSetupVersionless.sh` packages.

--- environmentSetupVersionless.sh
+++ environmentSetupVersionless.sh
@@ -1,3 +1,3 @@
 conda config --prepend channels conda-forge
 conda create -n OSMnxPyrLab --strict-channel-priority osmnx pyrosm jupyterlab
-conda install -c conda-forge jupyterlab_code_formatter black isort pytest
+conda install -c conda-forge jupyterlab_code_formatter black isort pylint pytest
