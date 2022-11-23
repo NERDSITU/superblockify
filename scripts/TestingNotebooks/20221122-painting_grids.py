5c3427e260505b1c5a0aa5b8bf0ac9368372f071
Carlson Moses Büth <c_buet05@uni-muenster.de>
2022-11-23 14:24:07 +0100

    Translated notebook to script.

---
+++ scripts/TestingNotebooks/20221122-painting_grids.py
@@ -0,0 +1,13 @@
+"""Script on how to use paint streets function"""
+# Belongs to 20221018-Painting_Grids notebook.
+
+import osmnx as ox
+
+from superblockify.plot import paint_streets
+
+if __name__ == "__main__":
+    CITY_NAME = 'Brooklyn_bearing.graphml'
+    graph = ox.load_graphml(filepath='../../tests/test_data/cities/' + CITY_NAME)
+
+    paint_streets(graph, save=True, edge_linewidth=0.5,
+                  filepath=f'../../tests/test_data/output/{CITY_NAME[:-8]}.pdf')
