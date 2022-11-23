41928c3768fdb3fed2b05eebc87502679bd88d78
Carlson Moses Büth <c_buet05@uni-muenster.de>
2022-11-23 14:42:43 +0100

    Change test paths.

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -6,42 +6,43 @@ import pytest
 
 from superblockify.plot import paint_streets
 
-
+TEST_DATA = "./tests/test_data/"
 @pytest.mark.parametrize(
     "city_path",
-    [city for city in listdir("./test_data/cities/") if city.endswith(".graphml")],
+    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(
+        ".graphml")],
 )
 @pytest.mark.parametrize("e_l,n_a", [(0.5, 0.5), (1, 0)])
 @pytest.mark.parametrize("save", [True, False])
 def test_paint_streets(city_path, e_l, n_a, save):
     """Test `paint_streets` by design."""
-    graph = ox.load_graphml(filepath="./test_data/cities/" + city_path)
+    graph = ox.load_graphml(filepath=f"{TEST_DATA}cities/" + city_path)
     paint_streets(
         graph,
         edge_linewidth=e_l,
         node_alpha=n_a,
         save=save,
-        filepath=f"./test_data/output/{city_path[:-8]}.pdf",
+        filepath=f"{TEST_DATA}output/{city_path[:-8]}.pdf",
     )
 
 
 @pytest.mark.parametrize(
     "city_path",
-    [city for city in listdir("./test_data/cities/") if city.endswith(".graphml")][:2],
+    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")][:2],
 )
 def test_paint_streets_overwrite_ec(city_path):
     """Test `paint_streets` trying to overwrite the edge colors."""
-    graph = ox.load_graphml(filepath="./test_data/cities/" + city_path)
+    graph = ox.load_graphml(filepath=f"{TEST_DATA}cities/" + city_path)
     with pytest.raises(ValueError):
         paint_streets(graph, edge_color="white")
 
 
 @pytest.mark.parametrize(
     "city_path",
-    [city for city in listdir("./test_data/cities/") if city.endswith(".graphml")][:2],
+    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")][:2],
 )
 def test_paint_streets_empty_plot(city_path):
     """Test `paint_streets` trying plot empty plot."""
-    graph = ox.load_graphml(filepath="./test_data/cities/" + city_path)
+    graph = ox.load_graphml(filepath=f"{TEST_DATA}cities/" + city_path)
     with pytest.raises(ValueError):
         paint_streets(graph, edge_linewidth=0, node_size=0)
