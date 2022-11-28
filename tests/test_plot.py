ecff396fbdc676df2227d6cb990b4730daecdce4
Carlson Moses Büth <c_buet05@uni-muenster.de>
2022-11-28 13:16:23 +0100

    Split up `paint_streets` function+tests to add function plotting by any edge attribute.

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -4,7 +4,8 @@ from os import listdir
 import osmnx as ox
 import pytest
 
-from superblockify.plot import paint_streets
+from superblockify import new_edge_attribute_by_function
+from superblockify.plot import paint_streets, plot_by_attribute
 
 TEST_DATA = "./tests/test_data/"
 
@@ -29,7 +30,7 @@ def test_paint_streets(city_path, e_l, n_a, save):
 
 @pytest.mark.parametrize(
     "city_path",
-    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")][:2],
+    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")],
 )
 def test_paint_streets_overwrite_ec(city_path):
     """Test `paint_streets` trying to overwrite the edge colors."""
@@ -40,10 +41,41 @@ def test_paint_streets_overwrite_ec(city_path):
 
 @pytest.mark.parametrize(
     "city_path",
-    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")][:2],
+    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")],
 )
 def test_paint_streets_empty_plot(city_path):
     """Test `paint_streets` trying plot empty plot."""
     graph = ox.load_graphml(filepath=f"{TEST_DATA}cities/" + city_path)
     with pytest.raises(ValueError):
         paint_streets(graph, edge_linewidth=0, node_size=0)
+
+
+@pytest.mark.parametrize(
+    "city_path",
+    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")],
+)
+def test_plot_by_attribute(city_path):
+    """Test `plot_by_attribute` by design."""
+    graph = ox.load_graphml(filepath=f"{TEST_DATA}cities/" + city_path)
+
+    # Use osmid as attribute determining color
+    # Some osmid attributes return lists, not ints, just take first element
+    new_edge_attribute_by_function(
+        graph,
+        lambda osmid: osmid if isinstance(osmid, int) else osmid[0],
+        'osmid',
+        'osmid_0'
+    )
+
+    plot_by_attribute(graph, 'osmid_0', cmap="rainbow")
+
+
+@pytest.mark.parametrize(
+    "city_path",
+    [city for city in listdir(f"{TEST_DATA}cities/") if city.endswith(".graphml")],
+)
+def test_plot_by_attribute_no_attribute(city_path):
+    """Test `plot_by_attribute` with missing attribute."""
+    graph = ox.load_graphml(filepath=f"{TEST_DATA}cities/" + city_path)
+    with pytest.raises(ValueError):
+        plot_by_attribute(graph, 'non_existent_attribute')
