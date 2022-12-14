e462cd81f635e1c9c47e53e7f8889ddd6e12fde1
Carlson Moses Büth <c_buet05@uni-muenster.de>
2022-12-14 12:13:53 +0100

    Changed `black` code formatter to the server version `blackd` for usage in IDEs.

--- environmentSetupVersionless.sh
+++ environmentSetupVersionless.sh
@@ -1,3 +1,3 @@
 conda create -n OSMnxPyrLab -c conda-forge osmnx pyrosm jupyterlab
 conda activate OSMnxPyrLab
-conda install -c conda-forge jupyterlab_code_formatter black isort pylint pytest coverage sphinx sphinx_rtd_theme
+conda install -c conda-forge jupyterlab_code_formatter blackd isort pylint pytest coverage sphinx sphinx_rtd_theme
