fb704179de5b378bb7b3f78d613631e6102fba83
Carlson Moses Büth <c_buet05@uni-muenster.de>
2023-02-15 17:47:53 +0100

    Tests for the Metric class Parametrized for all partitioners and length weighted test graphs.

---
+++ tests/test_metrics.py
@@ -0,0 +1,52 @@
+"""Tests for the metrics module."""
+from superblockify.metrics import Metric
+
+
+class TestMetric:
+    """Class to test the Metric class."""
+
+    def test_init(self):
+        """Test the init method."""
+        metric = Metric()
+        assert metric.coverage is None
+        assert metric.num_components is None
+        assert metric.avg_path_length == {"E": None, "S": None, "N": None}
+        assert metric.directness == {"ES": None, "EN": None, "SN": None}
+        assert metric.global_efficiency == {"SE": None, "NE": None, "NS": None}
+        assert metric.local_efficiency == {"SE": None, "NE": None, "NS": None}
+        assert metric.distance_matrix is None
+
+    def test_str(self):
+        """Test the __str__ method."""
+        metric = Metric()
+        assert str(metric) == ""
+        metric.coverage = 0.5
+        assert str(metric) == "coverage: 0.5; "
+        metric.num_components = 2
+        assert str(metric) == "coverage: 0.5; num_components: 2; "
+        metric.avg_path_length = {"E": None, "S": 4, "N": 11}
+        assert (
+            str(metric)
+            == "coverage: 0.5; num_components: 2; avg_path_length: S: 4, N: 11; "
+        )
+
+    def test_repr(self):
+        """Test the __repr__ method."""
+        metric = Metric()
+        assert repr(metric) == "Metric()"
+        metric.coverage = 0.5
+        assert repr(metric) == "Metric(coverage: 0.5; )"
+        metric.num_components = 2
+        assert repr(metric) == "Metric(coverage: 0.5; num_components: 2; )"
+        metric.avg_path_length = {"E": None, "S": 4, "N": 11}
+        assert (
+            repr(metric) == "Metric(coverage: 0.5; num_components: 2; "
+            "avg_path_length: S: 4, N: 11; )"
+        )
+
+    def test_calculate_all(self, test_city_bearing_length, partitioner_class):
+        """Test the calculate_all method."""
+        city_name, graph = test_city_bearing_length
+        part = partitioner_class(graph, name=city_name)
+        part.run()
+        part.calculate_metrics()
