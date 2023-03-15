6ad1455d881bbb9ebc632850fbd6d856841ad87c
Carlson Moses Büth <c_buet05@uni-muenster.de>
2023-03-15 18:29:25 +0100

    Add example SLURM scripts for HPC

---
+++ scripts/slurm/mainHPC.py
@@ -0,0 +1,28 @@
+"""Main script for development."""
+# Print current working directory
+import os
+from ast import literal_eval
+from configparser import ConfigParser
+
+print(f"Current working directory: {os.getcwd()}")
+
+from superblockify import ResidentialPartitioner
+
+config = ConfigParser()
+config.read("config.ini")
+PLACES_GENERAL = literal_eval(config["tests"]["places_general"])
+PLACES_SMALL = literal_eval(config["tests"]["places_small"])
+
+if __name__ == "__main__":
+    CITY_NAME, SEARCH_STR = PLACES_GENERAL[1]
+    # CITY_NAME, SEARCH_STR = PLACES_SMALL[0]
+
+    part = ResidentialPartitioner(
+        name=CITY_NAME + "_test", city_name=CITY_NAME, search_str=SEARCH_STR
+    )
+    part.run(make_plots=True)
+    part.save()
+    part = ResidentialPartitioner.load(name=CITY_NAME + "_test")
+
+    part.calculate_metrics(make_plots=True, num_workers=40, chunk_size=4)
+    part.save(save_metrics=True, save_graph_copy=True)
