4445121e8a408b7c90b13c1ef240970b19b6fe41
cbueth <cbueth@users.noreply.github.com>
2023-03-22 20:30:44 +0000

    Fix code style issues with Black

--- superblockify/partitioning/dummy.py
+++ superblockify/partitioning/dummy.py
@@ -52,6 +52,5 @@ class DummyPartitioner(BasePartitioner):
         # For tests where the partitioner only uses partitions, not components
         self.components = None
 
-
         if make_plots:
             self.plot_component_graph()
