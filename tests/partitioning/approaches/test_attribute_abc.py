7064dcad077010f04e256a6b152f4c97fdc8af82
Carlson B <49326130+cbueth@users.noreply.github.com>
2023-05-18 18:05:50 +0200

    Remove test of removed __ini__subclass__ attribute
    The instance attribute attribute_lavel is not passed to the class init anymore, tests need to be removed.

--- tests/partitioning/approaches/test_attribute_abc.py
+++ tests/partitioning/approaches/test_attribute_abc.py
@@ -8,20 +8,16 @@ from superblockify.partitioning.approaches.attribute import AttributePartitioner
 class TestAttributePartitioner:
     """Tests for the abstract attribute-based partitioner."""
 
-    @pytest.mark.parametrize("attribute", [None, 1, 1.0, True, False, [], {}])
-    def test__init_subclass__faulty_attribute(self, attribute):
-        """If attribute is not a string, raise a ValueError."""
-        with pytest.raises(ValueError):
-            # pylint: disable=unused-variable, too-few-public-methods
-            class FaultyAttribute(AttributePartitioner, attribute=attribute):
-                """Faulty attribute partitioner."""
+
+
+
 
     def test_write_attribute_not_overwritten(self):
         """If write_attribute is not overwritten, raise a TypeError
         when instantiating the class."""
         with pytest.raises(TypeError):
             # pylint: disable=too-few-public-methods
-            class NotOverwritten(AttributePartitioner, attribute="test"):
+            class NotOverwritten(AttributePartitioner):
                 """Partitioner that does not overwrite write_attribute."""
 
             NotOverwritten()  # pylint: disable=abstract-class-instantiated
