750a39679cfd848bb35af6c7b9b2330d2abdef56
Carlson Moses Büth <49326130+cbueth@users.noreply.github.com>
2023-06-25 21:45:53 +0200

    100 cities for geopackage example

--- scripts/examples/save_to_gpkg.py
+++ scripts/examples/save_to_gpkg.py
@@ -1,12 +1,12 @@
 """Example of saving a partitioner to a geopackage."""
 
 import superblockify as sb
-from superblockify.config import logger, PLACES_GENERAL, PLACES_SMALL, PLACES_OTHER
+from superblockify.config import logger, PLACES_GENERAL, PLACES_SMALL, PLACES_100_CITIES
 
 if __name__ == "__main__":
     # CITY_NAME, SEARCH_STR = PLACES_GENERAL[2]
     CITY_NAME, SEARCH_STR = PLACES_SMALL[1]
-    # CITY_NAME, SEARCH_STR = PLACES_OTHER[1]
+    # CITY_NAME, SEARCH_STR = PLACES_100_CITIES[1]
 
     logger.info(
         "Running partitioner for %s with search string %s.", CITY_NAME, SEARCH_STR
