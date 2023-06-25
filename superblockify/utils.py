404ce35fc2608452a0b04e667bcec1ccaa822b45
Carlson Moses Büth <49326130+cbueth@users.noreply.github.com>
2023-06-25 21:00:25 +0200

    Bearings: Deactivate calculation - only used in deprecated `BearingPartitioner`

--- superblockify/utils.py
+++ superblockify/utils.py
@@ -112,7 +112,9 @@ def load_graph_from_place(save_as, search_string, add_population=False, **gfp_kw
     # Get graph - automatically adds distances before simplifying
     graph = ox.graph_from_polygon(mult_polygon.geometry.unary_union, **gfp_kwargs)
     # Add edge bearings - the precision >1 is important for binning
-    graph = ox.add_edge_bearings(graph, precision=2)
+    # graph = ox.add_edge_bearings(graph, precision=2)
+    # only needed for deprecated BearingPartitioner
+
     # Project to local UTM - coordinates can be used as
     graph = ox.project_graph(graph)
 
