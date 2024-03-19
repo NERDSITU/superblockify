73733f68e393c171d3658cc5b58422eb5c361e72
Carlson Büth <commit@cbueth.de>
2024-03-19 18:12:20 +0100

    Doc: Commented MWE - Smaller example place, for shorter execution time - Saving and loading

--- scripts/mwe.py
+++ scripts/mwe.py
@@ -4,8 +4,27 @@ from superblockify import ResidentialPartitioner
 
 if __name__ == "__main__":
     part = ResidentialPartitioner(
-        name="Milan_mwe", city_name="Milan", search_str="Milan, Lombardy, Italy"
+        name="Salerno_mwe",
+        city_name="Salerno",
+        search_str="Salern, Campania, Italy",
+        # reload_graph=True
     )
-    part.run(make_plots=True)
-    part.calculate_metrics(make_plots=True)
-    part.save()  # Save the partitioner to disk
+    # Find search strings via https://nominatim.openstreetmap.org/
+    # The smaller the place, the quicker the partitioning
+    # For large places sufficient memory is required
+
+    # Save the preprocessed graph to disk:
+    # part.save(save_graph_copy=True)
+
+    # If loading all previous steps are not needed:
+    # part = sb.ResidentialPartitioner.load("Milan_mwe")
+
+    part.run(
+        calculate_metrics=True,
+        make_plots=True,
+        replace_max_speeds=False,
+    )
+
+    # Save the partitioner to disk, with all attributes, without an extra copy of the
+    # graph. The original graph has been cached in the data/graphs folder.
+    part.save()
