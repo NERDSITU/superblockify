bbd18576271e7f9e657dd816312a82fdafbbdaa5
Carlson Büth <commit@cbueth.de>
2024-06-12 10:01:53 +0200

    Add `scipy` dependency explicitly - remove unused `pyarrow` - 1.0.0rc9

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "superblockify"
 copyright = "2023-2024, superblockify developers"
 author = "superblockify developers"
-release = "1.0.0"
+release = "1.0.0rc9"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- superblockify/_version.py
+++ superblockify/_version.py
@@ -1,3 +1,3 @@
 """superblockify package version."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.0rc9"
